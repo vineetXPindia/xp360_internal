10ae7d4c266a833f76e373622e8737be057cf1cd
vineetXPindia <vineet.singh@xpindia.co.in>
2024-11-27 16:19:23 +0530

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyAlTpveCNu66Ne9YEsbvBldpmT6AuNYNuk")
+    // GMSServices.provideAPIKey("AIzaSyAlTpveCNu66Ne9YEsbvBldpmT6AuNYNuk")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
